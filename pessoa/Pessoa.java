0ae4421553c895b6eff005340d91469c78e954dc
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-02 00:53:28 -0300

    feat: heritage part1

--- pessoa/Pessoa.java
+++ pessoa/Pessoa.java
@@ -1,12 +1,31 @@
  package pessoa;
 
  public class Pessoa {
-    String name;
-    int idade;
-    String cpf;
+   private String nome;
+   private int idade;
+   private String cpf;
 
-    String imprimirDadosDaPessoa() {
-      return "O nome da pessoa é "  + name;
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
-    }
+  public String getCpf() {
+    return cpf;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public int getIdade() {
+    return idade;
+  }
+  public void setName(String nome) {
+    this.nome = nome;
+  }
+  public String getName() {
+    return nome;
+  }
+
+  String imprimirDadosDaPessoa() {
+    return "O nome da pessoa é "  + nome;
+  }
 }
--- pessoa/TesteDePessoa.java
+++ pessoa/TesteDePessoa.java
@@ -7,17 +7,17 @@ public class TesteDePessoa {
     // instanciando => inicializar  
     Pessoa pessoa1 = new Pessoa();
 
-    pessoa1.name = "vittor";
-    pessoa1.idade = 10;
-    pessoa1.cpf = "3232132";
+    pessoa1.setName("Vittor"); 
+    pessoa1.setIdade(10);
+    pessoa1.setCpf("3232132");
 
     System.out.println(pessoa1.imprimirDadosDaPessoa());
 
     Pessoa pessoa2 = new Pessoa();
 
-    pessoa2.name = "ramos";
-    pessoa2.idade = 10;
-    pessoa2.cpf = "3232132";
+    pessoa2.setName("Ramos"); 
+    pessoa2.setIdade(10);
+    pessoa2.setCpf("3232132");
 
     System.out.println(pessoa2.imprimirDadosDaPessoa());
   }
