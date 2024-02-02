dd92bd8dedb313aeeff6955e665b40fd5035a4b2
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-02 14:55:50 -0300

    feat: create area method in class Triangle

--- application/Program.java
+++ application/Program.java
@@ -25,19 +25,16 @@ public class Program {
     y.b = sc.nextDouble();
     y.c = sc.nextDouble();
 
-    double p = (x.a + x.b + x.c) / 2.0;
-    double areaX = Math.sqrt(p * (p - x.a)  * (p - x.b) * (p - x.c));
-
-    p = (y.a + y.b +  y.c) / 2.0;
-    double areaY = Math.sqrt(p * (p - y.a)  * (p - y.b) * (p - y.c));
+    double areaX = x.area();  
+    double areaY = y.area();
 
     System.out.printf("Triangle X area: %.4f%n", areaX);
     System.out.printf("Triangle Y area: %.4f%n", areaY);
 
     if(areaX > areaY) {
-      System.out.println("Large area: " + areaX);
+      System.out.println("Large area: X");
     } else {
-      System.out.println("Large area: " + areaY);
+      System.out.println("Large area: Y");
     }
     
     sc.close();
