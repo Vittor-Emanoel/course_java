0fd22728e2348a90a14448093f328385a88ef96d
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-05 00:02:12 -0300

    feat: polymorphism

--- pessoa/Professor.java
+++ pessoa/Professor.java
@@ -1,5 +1,19 @@
 package pessoa;
 
-public class Professor {
+public class Professor extends Pessoa {
+
+  private double salario;
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  String imprimirDadosDaPessoa() {
+    return "você é professor e recebe: "  + salario;
+  }
   
 }
--- pessoa/TesteDePessoa.java
+++ pessoa/TesteDePessoa.java
@@ -5,21 +5,19 @@ public class TesteDePessoa {
   
   public static void main(String[] args) {
     // instanciando => inicializar  
-    Pessoa pessoa1 = new Pessoa();
+    Professor professor = new Professor();
+    professor.setName("Vittor"); 
+    professor.setIdade(10);
+    professor.setCpf("3232132");
+    professor.setSalario(50000);
 
-    pessoa1.setName("Vittor"); 
-    pessoa1.setIdade(10);
-    pessoa1.setCpf("3232132");
 
-    System.out.println(pessoa1.imprimirDadosDaPessoa());
-
-    Pessoa pessoa2 = new Pessoa();
-
-    pessoa2.setName("Ramos"); 
-    pessoa2.setIdade(10);
-    pessoa2.setCpf("3232132");
-
-    System.out.println(pessoa2.imprimirDadosDaPessoa());
+    Aluno aluno = new Aluno();
+    aluno.setName("Vittor"); 
+    aluno.setIdade(10);
+    aluno.setCpf("3232132");
+    aluno.setMatricula("adawdawd3");
+   
   }
   
 }
